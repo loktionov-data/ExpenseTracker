a238d47ba99394e42838bcacfedafa64c00094e3
loktiyur <loktiyur@cvut.cz>
2026-03-20 11:24:01 +0100

    Fixed dependencies and plugins

--- src/test/java/com/expensetracker/service/ExpenseServiceTest.java
+++ src/test/java/com/expensetracker/service/ExpenseServiceTest.java
@@ -1,7 +1,7 @@
 package com.expensetracker.service;
 
 import com.expensetracker.model.Expense;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.time.LocalDate;
